bcdfaeeb29201ebbea6363fd530d1c9b0a5ffb27
Ras Haile <105021283+Rashaileguevara@users.noreply.github.com>
2022-07-18 22:48:35 +0300

    Update _putchar.c

--- 0x13-more_singly_linked_lists/_putchar.c
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -1,13 +1,13 @@
+#include <unistd.h>
+
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-typedef struct listint_s
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+return (write(1, &c, 1));
+}
